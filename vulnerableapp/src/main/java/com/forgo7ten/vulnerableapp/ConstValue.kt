180df424631d60c859190721d9511fb8f17316ff
Forgo7ten <Forgo7ten2020@gmail.com>
2023-04-14 17:59:55 +0800

    feat: 添加 应用调试模式配置不当漏洞

--- vulnerableapp/src/main/java/com/forgo7ten/vulnerableapp/ConstValue.kt
+++ vulnerableapp/src/main/java/com/forgo7ten/vulnerableapp/ConstValue.kt
@@ -21,6 +21,9 @@ object ConstValue {
             add( Vulnerability( appContext.getString(R.string.allow_backup_config), "低危",
             "<h2>漏洞简介</h2>\n" + "<p>&#39;AndroidManifest.xml&#39;文件中&#39;android:allowBackup&#39;属性错误的设置为&#39;true&#39;,导致客户端应用数据可以被备份导出。</p>\n" + "<h2>漏洞复现</h2>\n" + "<p>执行adb命令：</p>\n" + "<pre><code>adb backup -apk -shared -all -f backup.ab com.forgo7ten.vulnerableapp\n" + "</code></pre>\n" + "<p>&nbsp;</p>\n",
             VulnerabilityShowActivity::class.java ) )
+            add( Vulnerability( appContext.getString(R.string.allow_debug_config), "低危",
+            "<h2>漏洞简介</h2>\n" + "<p>&#39;AndroidManifest.xml&#39;文件中&#39;android:debuggable&#39;属性错误的设置为&#39;true&#39;,导致客户端应用可以被任意调试。</p>\n",
+            VulnerabilityShowActivity::class.java ) )
             add( Vulnerability( appContext.getString(R.string.denial_service_title), "低危",
                     "<h2>漏洞简介</h2>\n" + "<p>当Activity等组件接收Intent、并且使用了<code>get...Extra()</code>等方法时候，如果没有对传入的数据进行判断和处理，攻击者可以通过传入序列化对象导致App崩溃。</p>\n" + "<h3>漏洞详情</h3>\n" + "<p>当<code>get...Extra()</code>接收到一个自定义的序列化对象Serializable或者Parcelable对象时，在其中的getSerializableExtra()、getParcelable()方法中，由于无法解析传入的序列化类（找不到该类）导致报错</p>\n" + "<h3>漏洞危害</h3>\n" + "<p>当应用组件对外暴露时，第三方应用向手机传入构造好的Intent可以导致应用崩溃，极度影响用户体验。</p>\n" + "<h3>修复建议</h3>\n" + "<ul>\n" + "<li>非必要到处组件设置exported属性为false</li>\n" + "<li>导出组件在getIntent()后，get…Extra()方法时<strong>注意异常捕获</strong></li>\n" + "<li>对导出的组件设置好权限控制，不让任意第三方应用访问。</li>\n" + "\n" + "</ul>",
                     DenialServiceActivity::class.java ) )
